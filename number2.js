625fc6bfa72ff2000f2603e570ae5a80bc971242
Junior <djuniorn2@gmail.com>
2021-10-14 13:13:55 +0700

    Add: Test number 2 - Logging feature

--- number2.js
+++ number2.js
@@ -5,11 +5,35 @@ const axios = require('axios');
 const apiKey = "7d60ad7e";
 const apiBaseUrl = "https://www.omdbapi.com";
 const hbs = require('hbs');
+const db = require('./db');
+
+db.serialize(function() {
+	db.run(`
+		CREATE TABLE IF NOT EXISTS logs (
+			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+			key varchar(20) NOT NULL default '',
+			url varchar(250)  NOT NULL default ''
+		);
+	`);
+});
+
+// db.close();
 
 const myAxios = axios.create({
 	baseURL: apiBaseUrl,
 	headers: { 'Accept': 'application/json' }
 });
+
+myAxios.interceptors.request.use(req => {
+	console.log('Starting Request', req);
+	const fullUrl = req.baseURL + myAxios.getUri(req);
+
+	const statement = db.prepare("INSERT INTO logs (key, url) VALUES(?, ?)");
+	statement.run(apiKey, fullUrl);
+	statement.finalize();
+
+	return req;
+})
 
 hbs.registerHelper('ifCond', function (v1, operator, v2, options) {
 	switch (operator) {
@@ -51,12 +75,12 @@ app.get('/search', async (req, res) => {
 	if (req.query.s) {
 		const params = new URLSearchParams({ apikey: apiKey, ...req.query });
 		const response = await myAxios.get('/', { params })
-			.catch(err => console.error(err.toJSON()));
+			.catch(err => console.error(err));
 		// res.json(response.data);
-		if (response.data && response.data.Response === 'True') {
+		if (response && response.data && response.data.Response === 'True') {
 			data = response.data;
 		} else {
-			console.log(response.data);
+			console.log(response);
 		}
 	}
 	res.render('search.html', data);
@@ -67,16 +91,29 @@ app.get('/detail', async (req, res) => {
 	if (req.query.i) {
 		const params = new URLSearchParams({ apikey: apiKey, i: req.query.i });
 		const response = await myAxios.get('/', { params })
-			.catch(err => console.error(err.toJSON()));
+			.catch(err => console.error(err));
 
-		if (response.data && response.data.Response === 'True') {
+		if (response && response.data && response.data.Response === 'True') {
 			data = response.data;
 		} else {
-			console.log(response.data);
+			console.log(response);
 		}
 		console.log(response.data);
 	}
 	res.render('detail.html', data);
+});
+
+app.get('/logs', async (req, res) => {
+	db.all("SELECT * FROM logs ORDER BY id DESC LIMIT 20", function(err, rows) {
+		let data = {};
+		if (err) {
+			data.error = err;
+		} else {
+			data.rows = rows;
+		}
+		res.render('log.html', data);
+	});
+
 });
 
 app.listen(port, () => {
